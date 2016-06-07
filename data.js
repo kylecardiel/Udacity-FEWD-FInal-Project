7c3215884d1bf23966c19508eeb3c901719dfb8d
Kyle Cardiel <kyle.cardiel@gmail.com>
2016-06-07 16:54:59 -0500

    Initial Repo Creation

---
+++ data.js
@@ -0,0 +1,38 @@
+{"museums":[
+  {
+    "name": "Art Institute of Chicago",
+    "address": "111 South Michigan Avenue",
+    "state": "IL",
+    "city": "Chicago"
+  },
+  {
+    "name": "Adler Planetarium",
+    "address": "1300 S. Lake Shore Drive",
+    "state": "IL",
+    "city": "Chicago"
+  },
+  {
+    "name": "Field Museum of Natural History",
+    "address": "1400 S. Lake Shore Drive",
+    "state": "IL",
+    "city": "Chicago"
+  },
+  {
+    "name": "Museum of Science and Industry",
+    "address": "5700 South Lake Shore Drive",
+    "state": "IL",
+    "city": "Chicago"
+  },
+  {
+    "name": "Chicago History Museum",
+    "address": "1601 North Clark Street",
+    "state": "IL",
+    "city": "Chicago"
+  },
+  {
+    "name": "Lucas Museum of Narrative Art",
+    "address": "1410 Museum Campus Dr",
+    "state": "IL",
+    "city": "Chicago"
+  }
+]}
